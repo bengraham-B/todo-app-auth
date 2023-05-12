031041a83e7d5c95a8d3ff470ce174cf0ad9aaa0
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-05-12 21:33:21 +0200

    Adding TODO functionality

--- server/controllers/todo.controllers.js
+++ server/controllers/todo.controllers.js
@@ -1,23 +1,32 @@
+const Todo = require("../models/todoModel")
+const mongoose = require('mongoose')
+const {cl} = require('goosefuncs')
+
+//TODO GET ALL TODOS
 const getTodos = (req, res) => {
     res.status(200).json({msg:"All Todos!"})
 }
 
+//TODO GET A TODO
 const getTodo = (req, res) => {
     const {_id}= req.params
     res.status(200).json({msg: _id})
 }
 
+//TODO CREATE A TODO
 const createTodo = (req, res) => {
     const {_id, details, completed} = req.body
     console.log(details)
     res.status(200).json({_id, details, completed})
 }
 
+//TODO DELETE TODO
 const deleteTodo = (req, res) => {
     const {id}= req.params
     res.status(200).json({msg: id})
 }
 
+//TODO UPDATE TODO
 const updateTodo = (req, res) => {
     const id = req.body.id
     res.status(200).json({msg: "Update Todo!"})
